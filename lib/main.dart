a5d4b676dc336abcc215737a918fce2ec229ab42
Eduardo <echavezv@utea.edu.pe>
2024-12-18 06:54:51 -0500

    HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF219ebc),
+      appBar: AppBar(
+        title: Text(
+          "Dice App",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Color(0xFF023047),
+        centerTitle: true,
+        elevation: 10,
+      ),
     );
   }
 }
