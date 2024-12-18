58195e7fea1ef5abc17b9b586ac603a1b8df21da
Eduardo <echavezv@utea.edu.pe>
2024-12-18 07:30:40 -0500

    iamge recubierto con TextButton

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,26 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         elevation: 10,
       ),
+      body: Center(
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: TextButton(
+                  onPressed: () {
+                    print("Dado izquierdo");
+                  },
+                  child: Image.asset('assets/images/dice3.png')),
+            ),
+            Expanded(
+              flex: 1,
+              child: TextButton(
+                  onPressed: () {},
+                  child: Image.asset('assets/images/dice5.png')),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
